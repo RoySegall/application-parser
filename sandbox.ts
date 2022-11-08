39d0ea28bc7e5164f813a798ea0c0a1af9a46822
RoySegall <roy@testim.io>
2022-11-08 17:58:54 +0200

    Adding express as an example

--- sandbox.ts
+++ sandbox.ts
@@ -1,46 +1,9 @@
 import { resolve } from 'path';
-import * as StreamZip from 'node-stream-zip';
-import * as xml from 'fast-xml-parser';
-import * as BinaryXML from 'binary-xml';
-import {AndroidMetadata, Device, IOSMetadata} from "./types";
-import type {validationOptionsOptional} from "fast-xml-parser";
-
-function parseData(device: Device, xmlData: string | Buffer, validationOptions?: validationOptionsOptional | boolean): IOSMetadata | AndroidMetadata {
-    const xmlParser = new xml.XMLParser();
-    const parsedData = xmlParser.parse(xmlData);
-
-    if (device == 'ios') {
-        const {key: keys, string: values} = parsedData.plist.dict;
-        return Object.fromEntries(keys.map((key: string, i: number) => [key, values[i]])) as IOSMetadata
-    }
-
-    const reader = new BinaryXML(xmlData);
-    const data = reader.parse(xmlData);
-
-    const keyValuePairs = Object.values(data.attributes)
-        .map((attribute: {name: string, value: string}) => [attribute.name, attribute.value]);
-    return Object.fromEntries(keyValuePairs);
-}
-
-async function getApplicationMetadata(path: string, device: Device): Promise<IOSMetadata | AndroidMetadata> {
-    const zip = new StreamZip.async({ file: path });
-    const filesInZip = await zip.entries();
-
-    const pathToMetadata = Object.keys(filesInZip).find(file => file.includes(device === 'ios' ? 'Info.plist' : 'AndroidManifest.xml'))
-
-    if (!pathToMetadata) {
-        throw new Error('We could not find the metadata file path');
-    }
-
-    const data = await zip.entryData(pathToMetadata);
-    await zip.close();
-
-    return parseData(device, data);
-}
+import getApplicationMetadata from "./src";
 
 (async () => {
-    const ios = resolve(process.cwd(), 'demo-applications', 'ios.ipa');
-    const android = resolve(process.cwd(), 'demo-applications', 'android.apk');
+    const ios = resolve(process.cwd(), 'testing', 'demo-applications', 'ios.ipa');
+    const android = resolve(process.cwd(), 'testing', 'demo-applications', 'android.apk');
 
     const iosData = await getApplicationMetadata(ios, 'ios');
     const androidData = await getApplicationMetadata(android, 'android')
---
+++ testing/express/server.ts
@@ -0,0 +1,48 @@
+import * as express from 'express';
+import * as cors from 'cors';
+import {extname, resolve, basename} from 'path';
+import * as multer from 'multer';
+import getApplicationMetadata from "../../src";
+
+const app = express();
+const port = 3500;
+
+app.use(express.json());
+app.use(cors());
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, './uploads');
+    },
+    async filename(req, file, cb) {
+        const [filename, extension] = [basename(file.originalname), extname(file.originalname)];
+        const newFileName = `${filename}-${Date.now()}${extension}`;
+        req.body[file.fieldname] = newFileName;
+
+        cb(null, newFileName);
+    },
+});
+
+const upload = multer({ storage });
+
+app.post('/', upload.any(), async (req, res) => {
+
+    const results = await Promise.all(Object.entries(req.body).map(async (data: [string, string]) => {
+        const [field, filename] = data;
+        const filePath = resolve(process.cwd(), 'uploads', filename);
+
+        try {
+            return [field, await getApplicationMetadata(filePath, filename.endsWith('ipa') ? 'ios' : 'android')];
+        } catch (e) {
+            console.log(e);
+            return [];
+        }
+    }));
+
+    res.send(Object.fromEntries(results));
+});
+
+app.listen(port, () => {
+    // eslint-disable-next-line
+    console.log(`Example app listening on port ${port}`);
+});
